40ddea557d87827f83a653f62d97c02126c4e28b
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-08-27 16:38:20 -0400

    feat(main): handle response

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
+	"strconv"
 
 	"cloud.google.com/go/pubsub"
 	"google.golang.org/api/option"
@@ -13,18 +18,40 @@ var (
 	credentials = flag.String("credentials", "", "absolute path to the Google Credentials JSON file")
 	projectID   = flag.String("project", "", "ID of your Google Project where the topic is created")
 	topic       = flag.String("subscription", "", "name of the subscription")
+	slackURL    = flag.String("slackURL", "", "Incoming webhook URL for Slack")
 )
+
+type slackMessage struct {
+	Text string `json:"text"`
+	Md   bool   `json:"mrkdwn"`
+}
+
+type pubSubMessage struct {
+	BudgetDisplayName string `json:"budgetDisplayName"`
+	CostAmount        int    `json:"costAmount"`
+	CostIntervalStart string `json:"costIntervalStart"`
+	BudgetAmount      int    `json:"budgetAmount"`
+	BudgetAmountType  string `json:"budgetAmountType"`
+	CurrencyCode      string `json:"currencyCode"`
+}
 
 func isFlagGiven(flag *string) bool {
 	return len(*flag) > 0
+}
+
+func newSlackMessage(name, costAmount, budgetAmount, currencyCode string) *slackMessage {
+	return &slackMessage{
+		Text: fmt.Sprintf("*%s*\n_Cost amount:_ %s\n_Budget Amount:_ %s %s", name, costAmount, budgetAmount, currencyCode),
+		Md:   true,
+	}
 }
 
 func main() {
 
 	flag.Parse()
 
-	if !(isFlagGiven(credentials) || isFlagGiven(projectID) || isFlagGiven(topic)) {
-		log.Fatalf("unable to start pub-slack: -credentials, -project or -subscription is missing. --help to get informations.")
+	if !(isFlagGiven(credentials) || isFlagGiven(projectID) || isFlagGiven(topic) || isFlagGiven(slackURL)) {
+		log.Fatalf("unable to start pub-slack: -credentials, -project, -slackURL or -subscription is missing. --help to get informations.")
 	}
 
 	ctx := context.Background()
@@ -35,9 +62,43 @@ func main() {
 
 	sub := client.Subscription(*topic)
 	if err = sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
-		m.Ack()
+		if err = handleResponse(m.Data); err != nil {
+			log.Printf("unable to handle response from pub/sub: %v", err)
+		} else {
+			m.Ack()
+		}
 	}); err != nil {
 		log.Fatalf("unable to get message from topic: %v", err)
 	}
 
+}
+
+func handleResponse(data []byte) error {
+
+	var pubsub pubSubMessage
+	log.Println(string(data))
+	if err := json.Unmarshal(data, &pubsub); err != nil {
+		return fmt.Errorf("unable to extract json from google response: %v", err)
+	}
+
+	log.Println(pubsub.CurrencyCode)
+
+	msg := newSlackMessage(
+		pubsub.BudgetDisplayName,
+		strconv.Itoa(pubsub.CostAmount),
+		strconv.Itoa(pubsub.BudgetAmount),
+		pubsub.CurrencyCode,
+	)
+
+	payload, err := json.Marshal(msg)
+	if err != nil {
+		return fmt.Errorf("unable to create payload: %v", err)
+	}
+
+	resp, err := http.Post(*slackURL, "application/json", bytes.NewReader(payload))
+	if err != nil {
+		return fmt.Errorf("unable to post payload on slack URL: %v", err)
+	}
+	log.Println(resp)
+	return nil
 }
