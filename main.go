e4f49d109fa06fd97a3fa30db4b7b6351d0de5b8
Mathieu Tortuyaux <mathieu.tortuyaux@optelgroup.com>
2018-08-28 11:43:08 -0400

    feat(main): use json configuration

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"cloud.google.com/go/pubsub"
@@ -15,10 +17,10 @@ import (
 )
 
 var (
-	credentials = flag.String("credentials", "", "absolute path to the Google Credentials JSON file")
-	projectID   = flag.String("project", "", "ID of your Google Project where the topic is created")
-	topic       = flag.String("subscription", "", "name of the subscription")
-	slackURL    = flag.String("slackURL", "", "Incoming webhook URL for Slack")
+	credentials  = flag.String("credentials", "", "absolute path to the Google Credentials JSON file")
+	projectID    = flag.String("project", "", "ID of your Google Project where the subscription is created")
+	subscription = flag.String("subscription", "", "name of the subscription")
+	slackURL     = flag.String("slackURL", "", "Incoming webhook URL for Slack")
 )
 
 type slackMessage struct {
@@ -35,6 +37,13 @@ type pubSubMessage struct {
 	CurrencyCode      string  `json:"currencyCode"`
 }
 
+type configuration struct {
+	ProjectName  string `json:"project"`
+	Subscription string `json:"subscription"`
+	SlackURL     string `json:"slackURL"`
+	Credentials  string `json:"credentialsPath"`
+}
+
 func isFlagGiven(flag *string) bool {
 	return len(*flag) > 0
 }
@@ -46,34 +55,63 @@ func newSlackMessage(name, costAmount, budgetAmount, currencyCode string) *slack
 	}
 }
 
-func main() {
+func newConfiguration(projectName, subscription, slackURL, credentials string) *configuration {
+	return &configuration{
+		ProjectName:  projectName,
+		Subscription: subscription,
+		SlackURL:     slackURL,
+		Credentials:  credentials,
+	}
+}
+
+func parseArgs() *configuration {
 
 	flag.Parse()
 
-	if !(isFlagGiven(credentials) || isFlagGiven(projectID) || isFlagGiven(topic) || isFlagGiven(slackURL)) {
+	if !(isFlagGiven(credentials) || isFlagGiven(projectID) || isFlagGiven(subscription) || isFlagGiven(slackURL)) {
 		log.Fatalf("unable to start pub-slack: -credentials, -project, -slackURL or -subscription is missing. --help to get informations.")
+	}
+	return newConfiguration(*projectID, *subscription, *slackURL, *credentials)
+
+}
+
+func main() {
+	var pubConfiguration *configuration
+
+	_, err := os.Stat("./configuration.json")
+	if err != nil {
+		pubConfiguration = parseArgs()
+	} else {
+		conf, err := ioutil.ReadFile("./configuration.json")
+		if err != nil {
+			log.Fatalf("unable to load configuration from ./configuration.json: %v", err)
+		}
+		if err := json.Unmarshal(conf, &pubConfiguration); err != nil {
+			log.Fatalf("unable to unmarshal configuration: %v", err)
+		}
+
 	}
 
 	ctx := context.Background()
-	client, err := pubsub.NewClient(ctx, *projectID, option.WithCredentialsFile(*credentials))
+	client, err := pubsub.NewClient(ctx, pubConfiguration.ProjectName, option.WithCredentialsFile(pubConfiguration.Credentials))
 	if err != nil {
 		log.Fatalf("unable to create pub sub client: %v", err)
 	}
 
-	sub := client.Subscription(*topic)
+	sub := client.Subscription(pubConfiguration.Subscription)
 	if err = sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
-		if err = handleResponse(m.Data); err != nil {
+		if err = handleResponse(m.Data, pubConfiguration.SlackURL); err != nil {
 			log.Printf("unable to handle response from pub/sub: %v", err)
 		} else {
 			m.Ack()
 		}
 	}); err != nil {
-		log.Fatalf("unable to get message from topic: %v", err)
+		log.Fatalf("unable to get message from subscription: %v", err)
 	}
 
 }
 
-func handleResponse(data []byte) error {
+func handleResponse(data []byte, slackURL string) error {
 
 	var pubsub pubSubMessage
 	if err := json.Unmarshal(data, &pubsub); err != nil {
@@ -92,7 +130,7 @@ func handleResponse(data []byte) error {
 		return fmt.Errorf("unable to create payload: %v", err)
 	}
 
-	_, err = http.Post(*slackURL, "application/json", bytes.NewReader(payload))
+	_, err = http.Post(slackURL, "application/json", bytes.NewReader(payload))
 	if err != nil {
 		return fmt.Errorf("unable to post payload on slack URL: %v", err)
 	}
